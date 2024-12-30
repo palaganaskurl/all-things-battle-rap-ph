2228d1dc2072e19053bc719e867576fe1e19acd0
palaganaskurl <palaganaskurl@gmail.com>
2024-12-31 02:50:14 +0800

    feat: updated global font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-[family-name:var(--font-geist-sans)]`}
       >
         <NavigationMenu>
           <NavigationMenuList>
