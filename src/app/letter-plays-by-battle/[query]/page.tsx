df78cfd0c35e82b01a550fffa4fcc4a4390feb1d
Kurl Angelo Palaganas <palaganaskurl@gmail.com>
2025-03-06 04:54:30 +0800

    feat: added table footer on by battles table

--- src/app/letter-plays-by-battle/[query]/page.tsx
+++ src/app/letter-plays-by-battle/[query]/page.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/constants";
 import { Skeleton } from "@/components/ui/skeleton";
 import LetterPlaysByBattleTableSuspense from "./letter-plays-by-battle-table-suspense";
+import TableFooter from "@/components/custom/table-footer";
 
 export default async function LetterPlaysByBattlePage({
   params,
@@ -24,36 +25,41 @@ export default async function LetterPlaysByBattlePage({
 
   return (
     <div>
-      <Suspense
-        fallback={
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[30%]">{LetterPlayHeader}</TableHead>
-                <TableHead className="w-[50%]">
-                  {ExplanationContextHeader}
-                </TableHead>
-                <TableHead className="w-[20%]">{RapperHeader}</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell>
-                  <Skeleton className="h-[20px] rounded-full" />
-                </TableCell>
-                <TableCell>
-                  <Skeleton className="h-[20px] rounded-full" />
-                </TableCell>
-                <TableCell>
-                  <Skeleton className="h-[20px] rounded-full" />
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        }
-      >
-        <LetterPlaysByBattleTableSuspense query={query} />
-      </Suspense>
+      <div className="min-h-[calc(100vh-153px)] flex flex-col">
+        <Suspense
+          fallback={
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[30%]">{LetterPlayHeader}</TableHead>
+                  <TableHead className="w-[50%]">
+                    {ExplanationContextHeader}
+                  </TableHead>
+                  <TableHead className="w-[20%]">{RapperHeader}</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <Skeleton className="h-[20px] rounded-full" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="h-[20px] rounded-full" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="h-[20px] rounded-full" />
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          }
+        >
+          <LetterPlaysByBattleTableSuspense query={query} />
+        </Suspense>
+      </div>
+      <div>
+        <TableFooter />
+      </div>
     </div>
   );
 }
--- src/app/word-plays-by-battle/[query]/page.tsx
+++ src/app/word-plays-by-battle/[query]/page.tsx
@@ -14,6 +14,7 @@ import {
   WordPlayHeader,
 } from "@/constants";
 import { Skeleton } from "@/components/ui/skeleton";
+import TableFooter from "@/components/custom/table-footer";
 
 export default async function WordPlaysByBattlePage({
   params,
@@ -24,36 +25,41 @@ export default async function WordPlaysByBattlePage({
 
   return (
     <div>
-      <Suspense
-        fallback={
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[30%]">{WordPlayHeader}</TableHead>
-                <TableHead className="w-[50%]">
-                  {ExplanationContextHeader}
-                </TableHead>
-                <TableHead className="w-[20%]">{RapperHeader}</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell>
-                  <Skeleton className="h-[20px] rounded-full" />
-                </TableCell>
-                <TableCell>
-                  <Skeleton className="h-[20px] rounded-full" />
-                </TableCell>
-                <TableCell>
-                  <Skeleton className="h-[20px] rounded-full" />
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        }
-      >
-        <WordPlaysByBattleTableSuspense query={query} />
-      </Suspense>
+      <div className="min-h-[calc(100vh-153px)] flex flex-col">
+        <Suspense
+          fallback={
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[30%]">{WordPlayHeader}</TableHead>
+                  <TableHead className="w-[50%]">
+                    {ExplanationContextHeader}
+                  </TableHead>
+                  <TableHead className="w-[20%]">{RapperHeader}</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <Skeleton className="h-[20px] rounded-full" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="h-[20px] rounded-full" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="h-[20px] rounded-full" />
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          }
+        >
+          <WordPlaysByBattleTableSuspense query={query} />
+        </Suspense>
+      </div>
+      <div>
+        <TableFooter />
+      </div>
     </div>
   );
 }
